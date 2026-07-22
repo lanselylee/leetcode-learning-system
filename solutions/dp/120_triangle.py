b89cc8478cf731c304468bcb8c5aec53e6f583c9
Xiaomeng Li <xiaomengli@Xiaomengdebijibendiannao.local>
2026-07-21 23:29:50 -0700

    Add LC 120 Triangle to review queue

---
+++ solutions/dp/120_triangle.py
@@ -0,0 +1,19 @@
+"""
+LeetCode: 120. Triangle
+Approach: dp
+Link: https://leetcode.com/problems/triangle/
+Time: O(?)
+Space: O(?)
+"""
+
+from typing import List
+
+
+class Solution:
+    pass
+
+
+# Notes:
+# - Intuition:
+# - Edge cases:
+# - Mistakes:
