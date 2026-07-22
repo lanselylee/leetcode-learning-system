17e338b5610c5ee3297e3c0936739324ba3161b2
Xiaomeng Li <xiaomengli@Xiaomengdebijibendiannao.local>
2026-07-21 19:40:42 -0700

    Add LC 322 Coin Change and LC 300 LIS to review queue

---
+++ solutions/dp/300_longest_increasing_subsequence.py
@@ -0,0 +1,19 @@
+"""
+LeetCode: 300. Longest Increasing Subsequence
+Approach: dp
+Link: https://leetcode.com/problems/longest-increasing-subsequence/
+Time: O(?)
+Space: O(?)
+"""
+
+from typing import List
+
+
+class Solution:
+    pass
+
+
+# Notes:
+# - Intuition:
+# - Edge cases:
+# - Mistakes:
---
+++ solutions/dp/322_coin_change.py
@@ -0,0 +1,19 @@
+"""
+LeetCode: 322. Coin Change
+Approach: dp
+Link: https://leetcode.com/problems/coin-change/
+Time: O(?)
+Space: O(?)
+"""
+
+from typing import List
+
+
+class Solution:
+    pass
+
+
+# Notes:
+# - Intuition:
+# - Edge cases:
+# - Mistakes:
